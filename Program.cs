e2a5e0b9a3c11ff3ca25f0bfeb27ce057d5d5056
Nastia <titova_anastasia@tut.by>
2023-04-11 12:28:35 +0300

    Profram

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿string[] Print()
+    {
+    Console.Write("Введите массив строк через пробел: ");
+    string[] array = Console.ReadLine().Split();
+    return array;
+    }
+
+
+
+string[] array = Print();
+
+void GeneratArray(string[] array)
+{
+string[] newarray = new string[array.Length];
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <=3)
+    newarray[j] = array[i];
+    j++;
+}
+for (int i = 0; i < j; i++)
+        {
+            Console.Write(newarray[i] + " ");
+}
+}
+
+Console.WriteLine();
+Console.Write("Новый массив строк:");
+GeneratArray(array);
+Console.WriteLine();
